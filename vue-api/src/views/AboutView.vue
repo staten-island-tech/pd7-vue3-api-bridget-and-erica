4e138ad1b7fcc40dc6f90721a0ea1c1aa1d99fa0
bridgetf7 <fbridget0404@gmail.com>
2023-04-18 01:43:18 -0400

    vite/vue setup

--- vue-api/src/views/AboutView.vue
+++ vue-api/src/views/AboutView.vue
@@ -1,11 +1,15 @@
 <template>
-  <div></div>
+  <div class="about">
+    <h1>This is an about page</h1>
+  </div>
 </template>
 
-<script>
-export default {
-  // leave blank until ready to add routers
+<style>
+@media (min-width: 1024px) {
+  .about {
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+  }
 }
-</script>
-
-<style lang="scss" scoped></style>
+</style>
